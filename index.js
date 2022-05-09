2205eaa1e879b1dbbc8ea90757d9ee3d4a58530a
noman62 <noman.ice.nstu@gmail.com>
2022-05-09 01:03:30 -0700

    ready for deploy

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("hrEnterprise").collection("products");
-  const orderCollection=client.db("hrEnterprise").collection("orders");
+  const collection = client.db("manobotar-deyal").collection("products");
+  const orderCollection = client.db("manobotar-deyal").collection("orders");
+  const userCollection = client.db("manobotar-deyal").collection("users");
 
   app.get('/products', (req, res) => {
     collection.find()
@@ -38,6 +39,7 @@ client.connect(err => {
       })
   })
   app.post('/addProduct', (req, res) => {
+    console.log(req.body)
     const newEvent = req.body;
 
     collection.insertOne(newEvent)
@@ -64,8 +66,64 @@ client.connect(err => {
       })
   })
 
+  app.post('/register', (req, res) => {
+
+    //Received Data From Front-End
+    console.log(req.body);
+    const { name, email, password } = req.body
+
+    // Name validation
+    if (!name) return res.status(400).send('Name is required')
+
+    // Password validation
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send('Password is required and should be min 6 characters long')
+    }
+
+    // Email validation
+    let userExist = userCollection.findOne({ email })
+    if (userExist) {
+      return res.status(400).send('Email is taken')
+    } else {
+      // Save user in database
+      userCollection.insertOne({
+        name, email, password
+      })
+        .then(result => {
+
+          res.send(result.insertedCount > 0);
+        })
+    }
+
+  })
+
+  //User Login
+  app.post('/login',async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body
+    
+
+    // Email Validation
+    const user = await userCollection.findOne({ email });
+    console.log(user);
+    if (!user){
+      return res.status(400).send('No user found')
+    }
+console.log(user.password);
+    // Password Validation
+    if(user.password!==password){
+      return res.status(400).send("Password didn't match.")
+    }else{
+      return res.status(200).send("success")
+    }
+    
+  
+});
+
   app.get('/', (req, res) => {
-    res.send('HR ENTERPRISE !')
+    res.send('Manobotar Deyal!')
   })
   console.log('database connected');
 
